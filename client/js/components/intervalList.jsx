913840626bffd379a584cdfa2bd01f73e3f51334
hontas <lundin.pontus@gmail.com>
2015-12-02 11:27:57 +0100

    sort intervals descending on startedWorkingAt

--- client/js/components/intervalList.jsx
+++ client/js/components/intervalList.jsx
@@ -2,13 +2,19 @@ import React, { PropTypes } from 'react';
 import store from '../store/store';
 import IntervalListItem from './intervalListItem.jsx';
 
+function sortBy(array, prop) {
+  return array.slice().sort((a, b) => {
+    return b[prop] - a[prop];
+  });
+}
+
 export default React.createClass({
   propTypes: {
     intervals: PropTypes.arrayOf(PropTypes.object).isRequired
   },
 
   render() {
-    const children = this.props.intervals
+    const children = sortBy(this.props.intervals, 'startedWorkingAt')
       .map((interval) => <IntervalListItem { ...interval } key={ interval.id } onUpdate={ this.onUpdate } onDelete={ this.onDelete } />);
 
     return (
