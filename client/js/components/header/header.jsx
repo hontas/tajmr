7d9f7e1e1f8dec34161ee90f709fe60f638750c6
Pontus Lundin <lundin.pontus@gmail.com>
2015-12-05 22:49:19 +0100

    display version in upper right corner

--- client/js/components/header/header.jsx
+++ client/js/components/header/header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import pkg from '../../../../package.json';
 
 export default () => {
   return (
     <header className="header">
       <h1>{ 'TimR' }</h1>
+      <div className="version">{ pkg.version }</div>
     </header>
   );
 };
