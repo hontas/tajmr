96b26d442e9e227a781504bd6897a8b3aa844196
Pontus Lundin <lundin.pontus@gmail.com>
2015-12-10 20:49:35 +0100

    display intervals by day

--- client/js/components/intervalStats/intervalStats.jsx
+++ client/js/components/intervalStats/intervalStats.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { getTimePartsFromElapsedTime, zeroPad } from '../../utils/time';
 
-export default () => {
+const weekDays = ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'];
+const today = new Date();
+today.setDate(today.getDate() - 7);
+const aWeekBack = today.valueOf();
+const byOneWeekBack = (interval) => interval.startTime > aWeekBack;
+const getDayInterval = (interval) => ({
+  date: new Date(interval.startTime),
+  weekDay: weekDays[new Date(interval.startTime).getDay()],
+  interval: (interval.endTime || Date.now()) - interval.startTime
+});
+const collectByDate = (res, curr) => {
+  const key = curr.date.toLocaleDateString();
+  if (res[key]) {
+    res[key].interval += curr.interval;
+  } else {
+    res[key] = curr;
+  }
+
+  return res;
+};
+
+const IntervalStats = ({ intervals }) => {
+  const dateMap = intervals
+    .filter(byOneWeekBack)
+    .map(getDayInterval)
+    .reduce(collectByDate, {});
+
+  const intervalsDayList = Object.keys(dateMap)
+    .map((date) => {
+      const { hours, minutes } = getTimePartsFromElapsedTime(dateMap[date].interval);
+      const timestring = `${zeroPad(hours)}:${zeroPad(minutes)}`;
+      return <li key={ date }>{ dateMap[date].weekDay }{ ' - ' }{ timestring }</li>;
+    });
+
   return (
-    <div>
-      { 'intervalStats' }
-    </div>
+    <ul>
+      { intervalsDayList }
+    </ul>
   );
 };
+
+IntervalStats.propTypes = {
+  intervals: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+export default IntervalStats;
