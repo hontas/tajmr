31da547aec2f756fcddbf7745def11a2f0324f6b
Pontus Lundin <lundin.pontus@gmail.com>
2016-01-25 22:22:33 +0100

    disable localStorage

--- client/js/store/index.js
+++ client/js/store/index.js
@@ -1,13 +1,13 @@
 import { createStore } from 'redux';
 import reducers from '../reducers';
-import localStorage from '../utils/localStorage';
+//import localStorage from '../utils/localStorage';
 
-const version = '0.1.0';
-const initialState = localStorage.get(version);
+//const version = '0.1.0';
+const initialState = {};//localStorage.get(version);
 const store = createStore(reducers, initialState);
 
 store.subscribe(() => {
-  localStorage.set(version, store.getState());
+  //localStorage.set(version, store.getState());
 });
 
 export default store;
