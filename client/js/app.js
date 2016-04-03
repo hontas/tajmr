f8d79c7a2b9a5e90fe4e5fde5019f5c3e7199086
Pontus Lundin <lundin.pontus@gmail.com>
2016-04-03 12:43:49 +0200

    removed https

--- client/js/app.js
+++ client/js/app.js
@@ -6,11 +6,10 @@ import firebaseApi from './utils/firebaseApi';
 import store from './store';
 import Application from './components/application/application.jsx';
 
+firebaseApi.init();
 ReactDOM.render(
   <Provider store={ store }>
       <Application />
   </Provider>,
   document.getElementById('root')
 );
-
-firebaseApi.init();
--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 'use strict';
 
-const fs = require('fs');
-const https = require('https');
 const config = require('exp-config');
+const app = require('./server/app');
 
 if (config.useDevServer) {
   process.env.MONGOLAB_URI = 'mongodb://localhost:27017/tajmr';
 }
 
-const app = require('./server/app');
-const options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.crt')
-};
-
 app.set('port', (process.env.PORT || 5000));
 
-https.createServer(options, app).listen(app.get('port'), () => {
+app.listen(app.get('port'), () => {
   console.log('https server listening on port: %s, node version: %s', app.get('port'), process.version); // eslint-disable-line no-console
 });
