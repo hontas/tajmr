c1580d00b0ce579ceae951234f985b6e380dc619
Pontus Lundin <lundin.pontus@gmail.com>
2016-04-03 12:52:35 +0200

    removed redux-logger

--- client/js/store/index.js
+++ client/js/store/index.js
@@ -1,19 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
 import reducers from '../reducers';
-//import localStorage from '../utils/localStorage';
 
-//const version = '0.1.0';
-//localStorage.get(version);
 const initialState = {};
 const store = createStore(reducers, initialState, compose(
-  applyMiddleware(thunkMiddleware, createLogger()),
+  applyMiddleware(thunkMiddleware),
   window.devToolsExtension ? window.devToolsExtension() : (f) => f
 ));
-
-// store.subscribe(() => {
-//   localStorage.set(version, store.getState());
-// });
 
 export default store;
