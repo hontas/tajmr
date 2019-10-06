9f8cc1dab0972d4ad5f119952a2d8a44c5ef18f0
hontas <lundin.pontus@gmail.com>
2019-10-06 16:04:32 +0200

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   moduleNameMapper: {
     '^.+\\.(css|styl)$': '/Users/pontahontas/Sites/dev/tajmr/client/styles/CSSStub.js'
   },
-  setupTestFrameworkScriptFile: './test/test-setup.js'
+  setupFilesAfterEnv: ['./test/test-setup.js']
 };
