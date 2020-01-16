5af16cb3802519ec1c274e57ea2aec6bf8f726ab
hontas <lundin.pontus@gmail.com>
2020-01-16 23:50:26 +0100

    disable axe in dev

--- client/js/app.js
+++ client/js/app.js
@@ -52,8 +52,8 @@ const App = () => (
 
 if (process.env.NODE_ENV !== 'production') {
   // eslint-disable-next-line global-require
-  const axe = require('react-axe');
-  axe(React, ReactDOM, 1000);
+  // const axe = require('react-axe');
+  // axe(React, ReactDOM, 1000);
   ReactDOM.render(<App />, document.getElementById('root'));
 } else {
   ReactDOM.render(<App />, document.getElementById('root'));
