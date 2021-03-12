c125c7d799a76f87994159b51b55328db2b6e3b1
hontas <lundin.pontus@gmail.com>
2021-03-13 00:27:06 +0100

    fix lint issue

--- client/js/redux/createStore.js
+++ client/js/redux/createStore.js
@@ -5,6 +5,6 @@ import reducers from '.';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
 const composed = composeEnhancers(applyMiddleware(thunkMiddleware));
 
-export default function(initialState = {}) {
+export default function storeCreator(initialState = {}) {
   return createStore(reducers, initialState, composed);
 }
