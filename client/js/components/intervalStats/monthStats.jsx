ce7a9309e9e076b877a678262a2a8f13c0d9489c
hontas <lundin.pontus@gmail.com>
2021-04-26 23:31:25 +0200

    convert intervals stats comp

--- client/js/components/intervalStats/monthStats.jsx
+++ client/js/components/intervalStats/monthStats.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import * as customPropTypes from '../../constants/propTypes';
 import ProgressBar from '../ui-elements/progressBar.jsx';
 import { oneHour, getMonth, getWorkDaysInMonth } from '../../utils/time';
 import { getIntervalSum } from '../../utils/intervals';
+
+import styles from './monthStats.module.css';
 
 const MonthStats = ({ hoursPerWeek, monthIntervals, timestamp }) => {
   const month = getMonth(timestamp);
@@ -13,23 +16,23 @@ const MonthStats = ({ hoursPerWeek, monthIntervals, timestamp }) => {
   const totalWorkHoursSoFar = (hoursPerWeek / 5) * getWorkDaysInMonth(monthSoFar);
 
   return (
-    <div className="month-stats">
-      <h3 className="month-stats__heading">Månad</h3>
+    <div className={styles.container}>
+      <h3 className={styles.heading}>Månad</h3>
       <ProgressBar progress={workedHoursInMonth} max={totalWorkHoursInMonth} />
-      <h3 className="month-stats__heading">Flex</h3>
+      <h3 className={styles.heading}>Flex</h3>
       <ProgressBar progress={workedHoursInMonth} max={totalWorkHoursSoFar} unit="h" onlyDelta />
     </div>
   );
 };
 
 MonthStats.defaultProps = {
-  hoursPerWeek: 40
+  hoursPerWeek: 40,
 };
 
 MonthStats.propTypes = {
   monthIntervals: customPropTypes.intervals.isRequired,
   hoursPerWeek: PropTypes.number,
-  timestamp: PropTypes.number.isRequired
+  timestamp: PropTypes.number.isRequired,
 };
 
 export default MonthStats;
