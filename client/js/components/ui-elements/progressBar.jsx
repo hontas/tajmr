9a0d2720e825ebe6630bf1f93a4f1cc47278f5e9
hontas <lundin.pontus@gmail.com>
2021-04-27 22:26:08 +0200

    convert progress bar

--- client/js/components/ui-elements/progressBar.jsx
+++ client/js/components/ui-elements/progressBar.jsx
@@ -4,28 +4,24 @@ import classNames from 'classnames';
 
 import { maxOneDecimal } from '../../utils/number';
 
-const ProgressBar = ({ progress, max, onlyDelta, unit = '' }) => {
+import styles from './progressBar.module.css';
+
+const ProgressBar = ({ progress, max }) => {
   const percentage = (progress / max).toFixed(2);
   const isOvertime = percentage > 1.0;
   const width = !isOvertime ? `${percentage * 100}%` : `${(1 / percentage) * 100}%`;
   const restWidth = !isOvertime ? `${(1 - percentage) * 100}%` : `${(1 - 1 / percentage) * 100}%`;
-  const delta = maxOneDecimal(Math.abs(max - progress));
-  const classes = classNames('progress-bar', {
-    'progress-bar--only-delta': onlyDelta
-  });
   const progressText = [progress, max].map(maxOneDecimal).join(' / ');
 
   return (
-    <div className={classes}>
-      <div style={{ flexBasis: width }} className="progress-bar__progress" />
+    <div className={styles.container}>
+      <div style={{ flexBasis: width }} className={styles.progress} />
       <div
         style={{ flexBasis: restWidth }}
-        className={classNames('progress-bar__rest', { 'progress-bar__overtime': isOvertime })}
+        className={classNames(styles.rest, { [styles.overtime]: isOvertime })}
       />
-      <div className="progress-bar__text">
-        <span className="progress-bar__text__progress">{progressText}</span>
-        {!!delta && !onlyDelta && ' | '}
-        {!!delta && <span className="progress-bar__text__delta">{`${delta}${unit}`}</span>}
+      <div className={styles.text}>
+        <span className={styles.progressText}>{progressText}</span>
       </div>
     </div>
   );
@@ -33,9 +29,7 @@ const ProgressBar = ({ progress, max, onlyDelta, unit = '' }) => {
 
 ProgressBar.propTypes = {
   progress: PropTypes.number.isRequired,
-  onlyDelta: PropTypes.bool,
-  unit: PropTypes.string,
-  max: PropTypes.number.isRequired
+  max: PropTypes.number.isRequired,
 };
 
 export default ProgressBar;
